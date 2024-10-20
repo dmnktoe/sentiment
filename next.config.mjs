a933338b1ff6774ff8c5620a46ff0d041ee7a269
Domenik Töfflinger <domenikt96@yahoo.de>
2024-10-20 15:59:13 +0200

    display data from strapi

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "cms.project-sentiment.org",
+                port: '',
+                pathname: "/uploads/**/*",
+            },
+        ],
+    }
+};
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,57 @@
 import Image from "next/image";
+import qs from "qs";
 
-export default function Home() {
+async function getArticles() {
+  const baseUrl = "https://cms.project-sentiment.org";
+  const path = "/api/articles";
+
+  const url = new URL(path, baseUrl);
+
+  url.search = qs.stringify({
+    populate: {
+      image: {
+        fields: ["alternativeText", "name", "url"],
+      },
+    },
+  });
+
+  const res = await fetch(url);
+
+  if (!res.ok) throw new Error("Failed to fetch articles");
+
+  const data = await res.json();
+  console.log(data);
+
+  return data;
+}
+
+async function getProjects() {
+  const baseUrl = process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:1337";
+  const path = "/api/projects";
+
+  const url = new URL(path, baseUrl);
+
+  url.search = qs.stringify({
+    populate: {
+      image: {
+        fields: ["alternativeText", "name", "url"],
+      },
+    },
+  });
+
+  const res = await fetch(url);
+
+  if (!res.ok) throw new Error("Failed to fetch projects");
+
+  const data = await res.json();
+  console.log(data);
+
+  return data;
+}
+
+export default async function Home() {
+  const articles = await getArticles();
+  const projects = await getProjects();
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
@@ -12,7 +63,8 @@ export default function Home() {
           height={38}
           priority
         />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+        <ol
+          className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
             Get started by editing{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
@@ -48,6 +100,47 @@ export default function Home() {
             Read our docs
           </a>
         </div>
+
+        <div>
+          <h1 className="font-bold">Articles</h1>
+          {articles.data.map((article) => (
+            <div key={article.id}>
+              <h2 className="mb-3 text-2xl">{article.title}</h2>
+              <h2 className="mb-3 text-xs">{article.publishedAt}</h2>
+              <div className="flex gap-2 align-top justify-items-start">
+                <Image
+                  width="100"
+                  height="100"
+                  src={'https://cms.project-sentiment.org' + article.image.url}
+                  alt={article.image.alternativeText}
+                />
+                <p>{article.description}</p>
+              </div>
+              <hr className="my-3"/>
+            </div>
+          ))}
+        </div>
+
+        <div>
+          <h1 className="font-bold">Projects</h1>
+          {projects.data.map((project) => (
+            <div key={project.id}>
+              <h2 className="mb-3 text-2xl">{project.title}</h2>
+              <h2 className="mb-3 text-xs">{project.publishedAt}</h2>
+              <div className="flex gap-2 align-top justify-items-start">
+                <Image
+                  width="100"
+                  height="100"
+                  src={'https://cms.project-sentiment.org' + project.image.url}
+                  alt={project.image.alternativeText}
+                />
+                <p>{project.description}</p>
+              </div>
+              <hr className="my-3"/>
+            </div>
+          ))}
+        </div>
+
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
