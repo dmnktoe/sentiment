2decf68efa1c04700df2442db99b6ad1defc788c
Domenik Töfflinger <domenikt96@yahoo.de>
2024-11-15 14:41:48 +0100

    chore: update meta title and description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,8 +7,8 @@ import { Inter } from 'next/font/google';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: `TypeScript starter for Next.js by João Pedro Schmitz`,
-  description: `TypeScript starter for Next.js that includes all you need to build amazing apps`,
+  title: `SENTIMENT - IT Security Protects Privacy and Supports Democracy`,
+  description: `Funding guideline "Platform Privacy - IT Security Protects Privacy and Supports Democracy" as part of the German government's research framework program on IT security "Digital. Secure. Sovereign"`,
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
