6fa80014b579690e8f169f168a5bb6e8219199b6
dmnktoe <domenikt96@yahoo.de>
2024-11-18 23:19:39 +0100

    ui: layout updates

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,23 +2,21 @@ import { Metadata } from 'next';
 
 import './globals.css';
 
-import { Inter } from 'next/font/google';
+import React from 'react';
 
 import Footer from '@/components/layout/Footer';
 import Header from '@/components/layout/Header';
 import VisualGrid from '@/components/layout/VisualGrid';
 
-const inter = Inter({ subsets: ['latin'] });
-
 export const metadata: Metadata = {
   title: `SENTIMENT - IT Security Protects Privacy and Supports Democracy`,
-  description: `Funding guideline "Platform Privacy - IT Security Protects Privacy and Supports Democracy" as part of the German government's research framework program on IT security "Digital. Secure. Sovereign"`,
+  description: `Part of the German government's research framework program on IT security "Digital. Secure. Sovereign".`,
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='en'>
-      <body className='font-primary text-dark'>
+      <body>
         <Header />
         {children}
         <Footer />
