ea5a22db560246dbaf59840c5aba083be46d0535
dmnktoe <domenikt96@yahoo.de>
2024-11-19 16:38:04 +0100

    chore: create `Image` model type

---
+++ src/types/Image.ts
@@ -0,0 +1,42 @@
+type ImageFormats = {
+  large: ImageFormat;
+  small: ImageFormat;
+  medium: ImageFormat;
+  thumbnail: ImageFormat;
+};
+
+type ImageFormat = {
+  ext: string;
+  url: string;
+  hash: string;
+  mime: string;
+  name: string;
+  path: string | null;
+  size: number;
+  width: number;
+  height: number;
+};
+
+type ImageAttributes = {
+  name: string;
+  alternativeText: string | null;
+  caption: string | null;
+  width: number;
+  height: number;
+  formats: ImageFormats;
+  hash: string;
+  ext: string;
+  mime: string;
+  size: number;
+  url: string;
+  previewUrl: string | null;
+  provider: string;
+  provider_metadata: string | null;
+  createdAt: string;
+  updatedAt: string;
+};
+
+export type ImageData = {
+  id: number;
+  attributes: ImageAttributes;
+};
