40b76eec4ecba8084f337ac9c6075167f7edc35f
dmnktoe <domenikt96@yahoo.de>
2025-04-06 21:23:10 +0200

    style: resize crossbar

--- src/components/templates/Crossbar.tsx
+++ src/components/templates/Crossbar.tsx
@@ -1,7 +1,7 @@
 export default function Crossbar() {
   return (
     <div className='text-md mb-4 grid grid-cols-3 text-primary sm:grid-cols-4'>
-      <div className='-ml-[4.2923px]'>+</div>
+      <div className='-ml-[7.7923px] text-3xl'>+</div>
     </div>
   );
 }
