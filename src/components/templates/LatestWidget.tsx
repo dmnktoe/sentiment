0259684d959529bee3fe09e566241c7e60b334eb
dmnktoe <domenikt96@yahoo.de>
2025-04-10 13:38:04 +0200

    fix: padding in `LatestWidget`

--- src/components/templates/LatestWidget.tsx
+++ src/components/templates/LatestWidget.tsx
@@ -15,32 +15,34 @@ export default function LatestWidget({ articles }: LatestWidgetProps) {
     <>
       <section className='border-t-solid rounded-tl-[5rem] rounded-tr-[5rem] border-t-4 border-primary/30 py-24'>
         <Container>
-          <div className='mb-4 grid grid-cols-6 gap-0'>
-            <div className='col-span-5'>
-              <Title size='two' renderAs='h2' margin={false}>
-                Recent Articles ({articles.length})
-              </Title>
+          <div className='px-2 sm:px-4'>
+            <div className='mb-4 grid grid-cols-6 gap-0'>
+              <div className='col-span-5'>
+                <Title size='two' renderAs='h2' margin={false}>
+                  Recent Articles ({articles.length})
+                </Title>
+              </div>
             </div>
-          </div>
-          <div className='flex flex-col gap-4'>
-            {articles.map((article, index) => (
-              <ArticleCard
-                key={index as Key}
-                title={article.title}
-                publishedAt={article.publishedAt}
-                slug={article.slug}
-                description={article.description}
-                tags={article.tags}
-                author={article.author}
-              />
-            ))}
-          </div>
-          <div className='mt-8 flex justify-start'>
-            <Link href={`/articles`} className='text-xl hover:underline'>
-              View{' '}
-              <span className='font-secondary italic text-primary'> all</span>{' '}
-              articles <span className='font-secondary italic'>»</span>
-            </Link>
+            <div className='flex flex-col gap-4'>
+              {articles.map((article, index) => (
+                <ArticleCard
+                  key={index as Key}
+                  title={article.title}
+                  publishedAt={article.publishedAt}
+                  slug={article.slug}
+                  description={article.description}
+                  tags={article.tags}
+                  author={article.author}
+                />
+              ))}
+            </div>
+            <div className='mt-8 flex justify-start'>
+              <Link href={`/articles`} className='text-xl hover:underline'>
+                View{' '}
+                <span className='font-secondary italic text-primary'> all</span>{' '}
+                articles <span className='font-secondary italic'>»</span>
+              </Link>
+            </div>
           </div>
         </Container>
       </section>
