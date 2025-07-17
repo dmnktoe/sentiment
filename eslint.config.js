7d0fc73bbb3b45cdafc0e29287555ff3b8294aa3
Domenik Töfflinger <domenikt96@yahoo.de>
2025-07-17 11:20:11 +0200

    chore: fix prettier eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,101 +1,112 @@
-const {
-    defineConfig,
-} = require("eslint/config");
+const { defineConfig } = require('eslint/config');
 
-const globals = require("globals");
-const typescriptEslint = require("@typescript-eslint/eslint-plugin");
-const simpleImportSort = require("eslint-plugin-simple-import-sort");
-const unusedImports = require("eslint-plugin-unused-imports");
-const js = require("@eslint/js");
+const globals = require('globals');
+const typescriptEslint = require('@typescript-eslint/eslint-plugin');
+const simpleImportSort = require('eslint-plugin-simple-import-sort');
+const unusedImports = require('eslint-plugin-unused-imports');
+const js = require('@eslint/js');
 
-const {
-    FlatCompat,
-} = require("@eslint/eslintrc");
+const { FlatCompat } = require('@eslint/eslintrc');
 
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
-module.exports = defineConfig([{
+module.exports = defineConfig([
+  {
     languageOptions: {
-        globals: {
-            ...globals.browser,
-            ...globals.node,
-            React: true,
-            JSX: true,
-        },
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        React: true,
+        JSX: true,
+      },
     },
 
     plugins: {
-        "@typescript-eslint": typescriptEslint,
-        "simple-import-sort": simpleImportSort,
-        "unused-imports": unusedImports,
+      '@typescript-eslint': typescriptEslint,
+      'simple-import-sort': simpleImportSort,
+      'unused-imports': unusedImports,
     },
 
     extends: compat.extends(
-        "eslint:recommended",
-        "next",
-        "next/core-web-vitals",
-        "plugin:@typescript-eslint/recommended",
-        "prettier",
-        "plugin:prettier/recommended",
+      'eslint:recommended',
+      'next',
+      'next/core-web-vitals',
+      'plugin:@typescript-eslint/recommended',
+      'prettier',
+      'plugin:prettier/recommended',
     ),
 
     rules: {
-        "no-unused-vars": "off",
-        "no-console": "warn",
-        "no-multi-spaces": "error",
-        "@typescript-eslint/explicit-module-boundary-types": "off",
-        "react/no-unescaped-entities": "off",
-        "object-curly-spacing": ["warn", "always"],
+      'no-unused-vars': 'off',
+      'no-console': 'warn',
+      'no-multi-spaces': 'error',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      'react/no-unescaped-entities': 'off',
+      'object-curly-spacing': ['warn', 'always'],
 
-        quotes: ["warn", "single", {
-            avoidEscape: true,
-        }],
+      quotes: [
+        'warn',
+        'single',
+        {
+          avoidEscape: true,
+        },
+      ],
 
-        "react/display-name": "off",
+      'react/display-name': 'off',
 
-        "react/jsx-curly-brace-presence": ["warn", {
-            props: "never",
-            children: "never",
-        }],
+      'react/jsx-curly-brace-presence': [
+        'warn',
+        {
+          props: 'never',
+          children: 'never',
+        },
+      ],
 
-        "@typescript-eslint/no-unused-vars": "off",
-        "unused-imports/no-unused-imports": "warn",
+      '@typescript-eslint/no-unused-vars': 'off',
+      'unused-imports/no-unused-imports': 'warn',
 
-        "unused-imports/no-unused-vars": ["warn", {
-            vars: "all",
-            varsIgnorePattern: "^_",
-            args: "after-used",
-            argsIgnorePattern: "^_",
-        }],
+      'unused-imports/no-unused-vars': [
+        'warn',
+        {
+          vars: 'all',
+          varsIgnorePattern: '^_',
+          args: 'after-used',
+          argsIgnorePattern: '^_',
+        },
+      ],
 
-        "simple-import-sort/exports": "warn",
+      'simple-import-sort/exports': 'warn',
 
-        "simple-import-sort/imports": ["warn", {
-            groups: [
-                ["^@?\\w", "^\\u0000"],
-                ["^.+\\.s?css$"],
-                ["^@/lib", "^@/hooks"],
-                ["^@/data"],
-                ["^@/components", "^@/container"],
-                ["^@/store"],
-                ["^@/"],
-                [
-                    "^\\./?$",
-                    "^\\.(?!/?$)",
-                    "^\\.\\./?$",
-                    "^\\.\\.(?!/?$)",
-                    "^\\.\\./\\.\\./?$",
-                    "^\\.\\./\\.\\.(?!/?$)",
-                    "^\\.\\./\\.\\./\\.\\./?$",
-                    "^\\.\\./\\.\\./\\.\\.(?!/?$)",
-                ],
-                ["^@/types"],
-                ["^"],
+      'simple-import-sort/imports': [
+        'warn',
+        {
+          groups: [
+            ['^@?\\w', '^\\u0000'],
+            ['^.+\\.s?css$'],
+            ['^@/lib', '^@/hooks'],
+            ['^@/data'],
+            ['^@/components', '^@/container'],
+            ['^@/store'],
+            ['^@/'],
+            [
+              '^\\./?$',
+              '^\\.(?!/?$)',
+              '^\\.\\./?$',
+              '^\\.\\.(?!/?$)',
+              '^\\.\\./\\.\\./?$',
+              '^\\.\\./\\.\\.(?!/?$)',
+              '^\\.\\./\\.\\./\\.\\./?$',
+              '^\\.\\./\\.\\./\\.\\.(?!/?$)',
             ],
-        }],
+            ['^@/types'],
+            ['^'],
+          ],
+        },
+      ],
     },
-}]);
+  },
+]);
